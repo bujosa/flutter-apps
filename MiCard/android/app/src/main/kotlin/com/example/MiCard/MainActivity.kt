c6c349ae2a761b5d48b8e4bfd817cc25b47ad3ca
David <davidbujosa@gmail.com>
2020-08-12 17:27:47 -0400

    add

---
+++ MiCard/android/app/src/main/kotlin/com/example/MiCard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.MiCard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ MiCard/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: Container(),
+    ),
+  ));
+}
